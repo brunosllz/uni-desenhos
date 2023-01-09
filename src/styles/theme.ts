0c59627387d6594c0afc2b7645129190d69151be
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-01-09 10:54:31 -0300

    styles: add more gray color and sizes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -7,11 +7,14 @@ export const THEME = extendTheme({
       100: "#E1E1E6",
       200: "#C4C4CC",
       500: "#7C7C8A",
+      600: "#29292E",
       700: "#202024",
       900: "#121214"
     },
     green: {
-      500: "#04D361"
+      300: "#04D361",
+      500: "#00B37E",
+      700: "#00875F"
     },
     orange: {
       500: "#FBA94C"
@@ -27,5 +30,8 @@ export const THEME = extendTheme({
     md: 16,
     lg: 20,
     xl: 24,
+  },
+  sizes: {
+    14: 56
   },
 })
