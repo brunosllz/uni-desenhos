e346b4037edf5f2906faf4245b14ef5a5f90e607
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-01-09 14:54:09 -0300

    refactor(barCode): implement scan barcode

--- src/screens/BarCode.tsx
+++ src/screens/BarCode.tsx
@@ -1,15 +1,48 @@
+import { BarCodeScanner } from 'expo-barcode-scanner';
 import { VStack, Text } from 'native-base'
+import { useState, useEffect } from 'react';
+import { StyleSheet, Button, Alert } from 'react-native'
 
 export function BarCode() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    async function getBarCodeScannerPermissions() {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    console.log(data)
+    console.log(type)
+    Alert.alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <VStack
       flex={1}
-      alignItems="center"
       justifyContent="center"
+      bg="gray.500"
     >
-      <Text>
-        BarCode
-      </Text>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </VStack>
-  )
+  );
 }
