444d105ef7a5f8350ae92d5392e78ee8c4fba79e
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-01-09 11:42:46 -0300

    refactor(home): add all component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,56 @@
-import { VStack, Text } from 'native-base'
+import { useState } from 'react'
+import {
+  VStack,
+  HStack,
+} from 'native-base'
+
+import { Header } from '../components/Header'
+import { Option } from '../components/Option'
+import { Order } from './Order'
+import { MyCode } from './MyCode'
+import { PageIdentifier } from '../components/PageIdentifier'
 
 export function Home() {
+  const [optionsSelected, setOptionsSelected] = useState<'order' | 'myCode'>('order')
+
   return (
     <VStack
       flex={1}
-      alignItems="center"
-      justifyContent="center"
+      bg="gray.900"
     >
-      <Text>
-        Hello World!
-      </Text>
-    </VStack>
+      <Header title='UNICASA' />
+      <VStack
+        flex={1}
+        px={6}
+      >
+        {
+          optionsSelected === "order"
+            ? <PageIdentifier title='Ordens' hasCount />
+            : <PageIdentifier title='Meu cod' />
+        }
+
+        <HStack
+          mt={4}
+          space={4}
+        >
+          <Option
+            title='ORDENS'
+            isSelected={optionsSelected === "order"}
+            onPress={() => setOptionsSelected('order')}
+          />
+          <Option
+            title='MEU COD'
+            isSelected={optionsSelected === "myCode"}
+            onPress={() => setOptionsSelected('myCode')}
+          />
+        </HStack>
+
+        {
+          optionsSelected === "order"
+            ? <Order />
+            : <MyCode />
+        }
+      </VStack>
+    </VStack >
   )
 }
