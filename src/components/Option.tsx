83f29a433b3cdbb256d3a0bee634f4419ce25e45
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-01-09 09:24:32 -0300

    feat: add option component

---
+++ src/components/Option.tsx
@@ -0,0 +1,25 @@
+import { Center, Text, Pressable, IPressableProps } from 'native-base'
+
+interface Props extends IPressableProps {
+  title: string
+  isSelected: boolean
+}
+
+export function Option({ title, isSelected = false, ...rest }: Props) {
+  return (
+    <Pressable flex={1} h={9} {...rest}>
+      <Center
+        h="full"
+        w="full"
+        bgColor="gray.700"
+        borderWidth={1}
+        borderColor={isSelected ? "green.500" : "transparent"}
+        rounded="sm"
+      >
+        <Text color={isSelected ? "green.500" : "gray.500"} fontFamily="heading" fontSize="xs">
+          {title}
+        </Text>
+      </Center>
+    </Pressable>
+  )
+}
