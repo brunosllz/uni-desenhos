9429b65309bbd72614a48888020bd5a950c4cd71
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-01-09 11:07:14 -0300

    refactor(app): add status bar customization

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from './src/styles/theme';
 import { Home } from './src/screens/Home';
 import { Loading } from './src/components/Loading';
@@ -13,6 +13,11 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   );
