7b1d50418bdf0a71dc5f43be3821609e2f892d50
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-01-09 11:35:05 -0300

    refactor(option): change style at pressable

--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -7,14 +7,19 @@ interface Props extends IPressableProps {
 
 export function Option({ title, isSelected = false, ...rest }: Props) {
   return (
-    <Pressable flex={1} h={9} {...rest}>
+    <Pressable
+      flex={1}
+      h={9}
+      bgColor="gray.700"
+      _pressed={{ bgColor: "gray.600" }}
+      borderWidth={1}
+      borderColor={isSelected ? "green.300" : "transparent"}
+      rounded="sm"
+      {...rest}
+    >
       <Center
         h="full"
         w="full"
-        bgColor="gray.700"
-        borderWidth={1}
-        borderColor={isSelected ? "green.300" : "transparent"}
-        rounded="sm"
       >
         <Text color={isSelected ? "green.300" : "gray.500"} fontFamily="heading" fontSize="xs">
           {title}
