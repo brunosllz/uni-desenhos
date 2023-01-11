0a81b4f6c4ef9ff642ee951230287567a86ff771
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-01-11 11:20:44 -0300

    refactor(bottomSheet): change componet to forward ref

--- src/components/BotttomSheet.tsx
+++ src/components/BotttomSheet.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useCallback } from 'react'
+import { useEffect, useCallback, forwardRef, useImperativeHandle } from 'react'
 import { View } from 'native-base'
 import { StyleSheet, Dimensions } from 'react-native'
 import { GestureDetector, Gesture } from 'react-native-gesture-handler'
@@ -10,72 +10,83 @@ import Animated, {
   withSpring,
 } from 'react-native-reanimated'
 
+type BottomSheetProps = {}
+export type BottomSheetRef = {
+  toScroll: (destination: number) => void
+}
+
 const { height: SCREEN_HEIGHT } = Dimensions.get('window')
 const MAX_TRANSLATE_Y = -SCREEN_HEIGHT / 1.5
 
-export function BottomSheet() {
-  const translateY = useSharedValue(0)
-  const context = useSharedValue({ y: 0 })
+export const BottomSheet = forwardRef<BottomSheetProps, BottomSheetRef>(
+  ({ }, ref) => {
+    const translateY = useSharedValue(0)
+    const context = useSharedValue({ y: 0 })
 
-  const scrollTo = useCallback(
-    (destination: number) => {
-      'worklet'
-      translateY.value = withSpring(destination, { damping: 50 })
-    },
-    [translateY],
-  )
-
-  const gesture = Gesture.Pan()
-    .onStart(() => {
-      context.value = { y: translateY.value }
-    })
-    .onUpdate((event) => {
-      translateY.value = event.translationY + context.value.y
-      translateY.value = Math.max(translateY.value, MAX_TRANSLATE_Y)
-    })
-    .onEnd(() => {
-      if (translateY.value > -SCREEN_HEIGHT / 5) {
-        scrollTo(0)
-      }
-
-      if (translateY.value < -SCREEN_HEIGHT / 2) {
-        scrollTo(MAX_TRANSLATE_Y)
-      }
-    })
-
-  const rBottomSheetStyle = useAnimatedStyle(() => {
-    const borderRadius = interpolate(
-      translateY.value,
-      [MAX_TRANSLATE_Y + 50, MAX_TRANSLATE_Y],
-      [35, 15],
-      Extrapolate.CLAMP,
+    const scrollTo = useCallback(
+      (destination: number) => {
+        'worklet'
+        translateY.value = withSpring(destination, { damping: 50 })
+      },
+      [translateY],
     )
 
-    return {
-      borderRadius,
-      transform: [{ translateY: translateY.value }],
-    }
-  })
+    useImperativeHandle(ref, () => ({ scrollTo }), [scrollTo])
 
-  useEffect(() => {
-    scrollTo(-SCREEN_HEIGHT / 1.5)
-  }, [scrollTo])
+    const gesture = Gesture.Pan()
+      .onStart(() => {
+        context.value = { y: translateY.value }
+      })
+      .onUpdate((event) => {
+        translateY.value = event.translationY + context.value.y
+        translateY.value = Math.max(translateY.value, MAX_TRANSLATE_Y)
+      })
+      .onEnd(() => {
+        if (translateY.value > -SCREEN_HEIGHT / 5) {
+          scrollTo(0)
+        }
 
-  return (
-    <GestureDetector gesture={gesture}>
-      <Animated.View style={[styles.container, rBottomSheetStyle]}>
-        <View
-          w={75}
-          h={1}
-          bg="gray.800"
-          alignSelf="center"
-          my={3}
-          rounded="full"
-        />
-      </Animated.View>
-    </GestureDetector>
-  )
-}
+        if (translateY.value < -SCREEN_HEIGHT / 2) {
+          scrollTo(MAX_TRANSLATE_Y)
+        }
+      })
+
+    const rBottomSheetStyle = useAnimatedStyle(() => {
+      const borderRadius = interpolate(
+        translateY.value,
+        [MAX_TRANSLATE_Y + 50, MAX_TRANSLATE_Y],
+        [35, 15],
+        Extrapolate.CLAMP,
+      )
+
+      return {
+        borderRadius,
+        transform: [{ translateY: translateY.value }],
+      }
+    })
+
+    useEffect(() => {
+      scrollTo(-SCREEN_HEIGHT / 1.5)
+    }, [scrollTo])
+
+    return (
+      <GestureDetector gesture={gesture}>
+        <Animated.View style={[styles.container, rBottomSheetStyle]}>
+          <View
+            w={75}
+            h={1}
+            bg="gray.800"
+            alignSelf="center"
+            my={3}
+            rounded="full"
+          />
+        </Animated.View>
+      </GestureDetector>
+    )
+  },
+)
+
+BottomSheet.displayName = 'BottomSheet'
 
 const styles = StyleSheet.create({
   container: {
