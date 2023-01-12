c998672dbc3f959902ef9cc994ce2200b57f6bd1
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-01-12 10:35:26 -0300

    refactor(orderCard): separate in two component one for show and other to make download

--- src/components/OrderCard.tsx
+++ src/components/OrderCard.tsx
@@ -1,21 +1,18 @@
 import { HStack, Text, VStack, Pressable, Icon } from 'native-base'
 import { Feather } from '@expo/vector-icons'
+import { OrderProps } from '../screens/Home/screens/Order'
 
 interface OrderCardProps {
-  icon?: 'download' | 'trash-2'
-  variant?: 'primary' | 'secondary'
+  order?: OrderProps
 }
 
-export function OrderCard({
-  icon = 'trash-2',
-  variant = 'primary',
-}: OrderCardProps) {
+export function OrderCard({ order }: OrderCardProps) {
   return (
     <HStack
       bg="gray.700"
       rounded="sm"
       borderLeftWidth={8}
-      borderColor={variant === 'primary' ? 'green.300' : 'orange.500'}
+      borderColor="green.300"
       p={5}
       alignItems="center"
       justifyContent="space-between"
@@ -23,15 +20,36 @@ export function OrderCard({
     >
       <VStack space={2} alignItems="flex-start">
         <Text color="gray.100" fontWeight="bold" fontSize="md">
-          Ordem 474564
+          Ordem 322979219
         </Text>
 
         <HStack space={2} alignItems="center">
           <Icon as={Feather} name="clock" color="gray.500" size="sm" />
-          <Text color="gray.500" fontSize="sm">
+          <Text
+            color="gray.500"
+            fontSize="sm"
+            w="70%"
+            numberOfLines={1}
+            ellipsizeMode="tail"
+          >
             20/01/22 às 14h
           </Text>
         </HStack>
+
+        {/* <HStack space={2} alignItems="center">
+          <Icon as={Feather} name="clock" color="gray.500" size="sm" />
+          {order.MASC.startsWith('CAMINHO') && (
+            <Text
+              color="gray.500"
+              fontSize="sm"
+              w="70%"
+              numberOfLines={1}
+              ellipsizeMode="tail"
+            >
+              Não possui desenho
+            </Text>
+          )}
+        </HStack> */}
       </VStack>
 
       <Pressable
@@ -42,12 +60,7 @@ export function OrderCard({
           bg: 'gray.500',
         }}
       >
-        <Icon
-          as={Feather}
-          name={icon}
-          color={variant === 'primary' ? 'orange.500' : 'green.500'}
-          size="lg"
-        />
+        <Icon as={Feather} name="trash-2" color="orange.500" size="lg" />
       </Pressable>
     </HStack>
   )
---
+++ src/components/OrderCardDownload.tsx
@@ -0,0 +1,69 @@
+import { HStack, Text, VStack, Pressable, Icon } from 'native-base'
+import { Feather } from '@expo/vector-icons'
+import { OrderProps } from '../screens/Home/screens/Order'
+
+interface OrderCardProps {
+  order: OrderProps
+}
+
+export function OrderCardDownload({ order }: OrderCardProps) {
+  return (
+    <HStack
+      bg="gray.700"
+      rounded="sm"
+      borderLeftWidth={8}
+      borderColor="orange.500"
+      p={5}
+      alignItems="center"
+      justifyContent="space-between"
+      mb={3}
+    >
+      <VStack space={2} alignItems="flex-start">
+        <Text color="gray.100" fontWeight="bold" fontSize="md">
+          Item {order.ITEM}
+        </Text>
+
+        <HStack space={2} alignItems="center">
+          {order.MASC.startsWith('CAMINHO') ? (
+            <>
+              <Icon as={Feather} name="x" color="red.500" size="sm" />
+              <Text
+                color="red.500"
+                fontSize="sm"
+                w="70%"
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                Não possui desenho
+              </Text>
+            </>
+          ) : (
+            <>
+              <Icon as={Feather} name="check" color="green.500" size="sm" />
+              <Text
+                color="green.500"
+                fontSize="sm"
+                w="70%"
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                Possui desenho
+              </Text>
+            </>
+          )}
+        </HStack>
+      </VStack>
+
+      <Pressable
+        p={4}
+        rounded="full"
+        bgColor="gray.600"
+        _pressed={{
+          bg: 'gray.500',
+        }}
+      >
+        <Icon as={Feather} name="download" color="green.500" size="lg" />
+      </Pressable>
+    </HStack>
+  )
+}
