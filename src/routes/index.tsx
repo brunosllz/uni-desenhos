a876f00c6a064e31c96d459bef8a64e80784796b
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-01-23 21:56:01 -0300

    chore: implement splash screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createStackNavigator } from '@react-navigation/stack'
+import { View } from 'native-base'
 import { BarCodeCamera } from '../screens/BarCodeCamera'
 import { Home } from '../screens/Home'
 import { MyCodeCamera } from '../screens/MyCodeCamera'
@@ -6,14 +7,16 @@ import { MyCodeCamera } from '../screens/MyCodeCamera'
 const { Navigator, Screen } = createStackNavigator()
 export function Routes() {
   return (
-    <Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Screen name="home" component={Home} />
-      <Screen name="barCodeCamera" component={BarCodeCamera} />
-      <Screen name="myCodeCamera" component={MyCodeCamera} />
-    </Navigator>
+    <View flex={1} bg="gray.900">
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Screen name="home" component={Home} />
+        <Screen name="barCodeCamera" component={BarCodeCamera} />
+        <Screen name="myCodeCamera" component={MyCodeCamera} />
+      </Navigator>
+    </View>
   )
 }
